656d8344b518332b50dbe2208335fd2a627ad54e
arturo <arturo.delreal95@gmail.com>
2025-04-21 11:41:29 +0200

    Add fly io deploy :)
    Add basic deploy for fly io, using a rds db and s3 storage.
    
    Add an example .env file.
    
    Set allowed hosts by env vars.
    
    Add fly config.
    
    Add bash script to allow upload env vars from a .env file.

--- Dockerfile
+++ Dockerfile
@@ -8,7 +8,6 @@ RUN pip install --no-cache-dir -r requirements.txt
 COPY . .
 
 # Run python needed commands. Probably should change for a bash script
-RUN python manage.py migrate
 RUN python manage.py collectstatic --noinput
 
 EXPOSE 8000
--- arriendos/settings.py
+++ arriendos/settings.py
@@ -28,8 +28,9 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == 'True'
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost").split(",")
 
+CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS", "").split(",")
 
 # Application definition
 
